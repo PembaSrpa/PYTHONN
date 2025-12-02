77abdc1f8c2737d93ffae81cf85ed68b04c373d7
PembaSrpa <nirashanichang@gmail.com>
2025-12-02 11:08:01 +0545

    installation

---
+++ 1.Installation/py.py
@@ -0,0 +1,70 @@
+'''
+# what exactly is python?
+Python is a high-level, interpreted, interactive and object-oriented scripting language. Python is designed to be highly readable.
+
+# Why learn python?
+Python is a versatile language that can be used for a wide range of applications, from web development to data science.
+
+# Python's root and it's ecosystem
+Python's root is Guido van Rossum. Python's ecosystem is a collection of tools and libraries that can be used to build applications.
+
+# Python's features
+1. Easy to learn
+2. Easy to read
+3. Easy to write
+4. Easy to debug
+5. Easy to test
+6. Easy to deploy
+7. Easy to maintain
+
+# Python's installation and IDEs setting up
+1. Download the latest version of Python from the official website: https://www.python.org/downloads/
+2. Run the installer and follow the instructions.
+3. Add Python to the PATH environment variable.
+4. Open a command prompt and type "python --version" to verify that Python is installed correctly.
+5. Install an IDE (Integrated Development Environment) like PyCharm, VSCode, etc.
+6. Open the IDE and create a new project.
+7. Create a new file and write your code in it.
+8. Run the code and see the output.
+
+# Python's Frameworks and Python Syntax
+1. Python's Frameworks are a collection of tools and libraries that can be used to build applications. Some popular frameworks are:
+    - Django (web development)
+    - Flask (web development)
+    - FastAPI (APIs)
+    - TensorFlow and PyTorch (machine learning)
+    - Pandas and NumPy (data analysis)
+    - Pygame (game development)
+    - Tkinter (desktop applications)
+
+2. Python Syntax refers to the rules of how Python code is written. Some basic examples are:
+    - Variables:
+        x = 10
+        name = "Artt"
+    - Conditions:
+        if x > 5:
+            print("Greater")
+    - Loops:
+        for i in range(5):
+            print(i)
+    - Functions:
+        def add(a, b):
+            return a + b
+    - Classes:
+        class Dog:
+            def __init__(self, name):
+                self.name = name
+    - Input/Output:
+        user = input("Enter name: ")
+        print(user)
+    - Comments:
+        # This is a comment
+
+# Hands on writing code on Google Colab
+1. Open the Google Colab website: https://colab.research.google.com/
+2. Create a new notebook.
+3. Write your code in the notebook.
+4. Run the code and see the output.
+5. Save the notebook and share it with others.
+
+'''
